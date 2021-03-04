cefcf598a376250d6221ff1c74366fb7152e9852
mukul <mukul_wana@yahoo.com>
2021-03-03 20:23:11 -0500

    cucumber code

---
+++ test-output-thread/report.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f6c99ab1-8589-41f4-92dc-7609b068fcb7",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1614797789337,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614797799067,
+    "className": "passed"
+  },
+  {
+    "id": "30ac728d-f119-4a74-b148-53a0d0051590",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1614797759204,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1614797769301,
+    "className": "passed"
+  },
+  {
+    "id": "af87003b-555e-4b9e-9ad9-9b2f8bba2f93",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1614797779392,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614797789311,
+    "className": "passed"
+  },
+  {
+    "id": "6bda5051-e9b0-42a9-ad7c-b7ce3c7e2e03",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1614797799088,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614797809253,
+    "className": "passed"
+  },
+  {
+    "id": "f64eedd8-b0ee-4ce1-badc-ce3afe4a4fbc",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1614797809266,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614797817713,
+    "className": "failed"
+  },
+  {
+    "id": "aeffbff5-db94-439c-9d76-896f9c07ef4c",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1614797817763,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614797825545,
+    "className": "passed"
+  },
+  {
+    "id": "911c42f0-5d87-4839-a515-1f0494435192",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1614797769341,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1614797779359,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
